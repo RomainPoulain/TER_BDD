370bf53a4784db76806400d0dea8c38e6fff6aea
RomainPoulain <romain.poulain.etu@univ-lille.fr>
2020-05-20 19:42:59 +0200

    fin

--- code/edit-distance.py
+++ code/edit-distance.py
@@ -6,24 +6,6 @@ import json
 from datetime import date, datetime
 from math import exp
 
-# '''
-# print(nltk.edit_distance('slawek'.lower(),'Sławek'.lower()))
-# print(nltk.edit_distance('slawek staworko','staworko slawek')) #8
-# print('slawek p. staworko, dr. hdr'.replace('.',' ').replace(',',' ').split()) #['slawek', 'p', 'staworko', 'dr', 'hdr']
-
-# print(nltk.jaccard_distance(set('slawek staworko'.split()), set('staworko slawek'.split()))) # 0.0
-#  # {'slawek, 'staworko'}
-#  # {'staworko', 'slawek'}
-
-
-# print(set('slawek staworko'))
-# #print(nltk.jaccard_distance(set('slawek staworko'.split()), set('staworko slawek'.split()))) # 0.0
-
-
-# sys.exit(1)
-
-# '''
-
 
 COLUMNS = ['type', 'key', 'revision', 'last_modified', 'json']
 INPUT_FILE = 'small-authors.txt'
@@ -54,11 +36,11 @@ result = c.execute(req)
 for i in range(0, 1000):
     try:
         tab_id2.append(c.fetchone()['ID'])
-    except:
-        TypeError
+    except TypeError:
+        None
 
 
-# '''
+# '''  VERSION AVEC LES DICTIONNAIRES
 # x = 0
 # liste = []
 # for x in range(len(names)):
@@ -106,10 +88,6 @@ def get_author_name(author_id):
     except TypeError:
         return None
 
-
-#print(get_author_name('/authors/OL1000057A'))
-#print(get_author_name('/about/oregon'))
-
 def get_author_alt_names(author_id):
     c.execute("SELECT NAME FROM AUTHOR_ALT_NAME WHERE ID_AUTHOR = ?", (author_id,))
     try:
@@ -117,10 +95,6 @@ def get_author_alt_names(author_id):
         return d
     except TypeError:
         return None
-
-
-#print(get_author_alt_names('/authors/OL1001542A'))
-
 
 def dist_author_name(author_id_1, author_id_2):
     result = 0
@@ -137,12 +111,11 @@ def dist_author_name(author_id_1, author_id_2):
         return ed2 / (abs(len(author_name_1)) + abs(len(author_alt_names_1)))
     try:
         result = ed / (abs(len(author_name_1)) + abs(len(author_name_2)))
-    except:
-        ZeroDivisionError
-    return result
+        return result
+    except ZeroDivisionError:
+        return None
 
 
-# print(dist_author_name('/authors/OL1000057A','/authors/OL1027674A'))
 
 def sigmoid(x):
     return 1 / (1 + exp(-x))
@@ -215,10 +188,6 @@ def dist_author_date(author_id_1, author_id_2):
 
 
 def dist_author(author_id_1, author_id_2):
-    author_name_1 = get_author_name(author_id_1)
-    author_name_2 = get_author_name(author_id_2)
-    author_date_1 = get_author_birth_date(author_id_1)
-    author_date_2 = get_author_birth_date(author_id_2)
     author_name = dist_author_name(author_id_1, author_id_2)
     author_date = dist_author_date(author_id_1, author_id_2)
     if author_name is None or author_date is None:
@@ -228,51 +197,13 @@ def dist_author(author_id_1, author_id_2):
     if author_name is None and author_date is not None:
         return author_date
     if author_name is not None and author_date is not None:
-        #print(author_name_1, "  ", author_name_2, "  ", author_date_1, "  ", author_date_2)
         return (author_name + author_date) / 2
 
 
-'''
-def dist_author(author_id_1, author_id_2):
-    if dist_author_name(author_id_1, author_id_2) < 0.3:
-        author_name_1 = get_author_name(author_id_1)
-        author_name_2 = get_author_name(author_id_2)
-        author_date_1 = get_author_birth_date(author_id_1)
-        author_date_2 = get_author_birth_date(author_id_2)
-        print("SIMILAIRE ",author_name_1, " ",author_name_2, " distance = ", dist_author_name(author_id_1, author_id_2))
-        if dist_author_date(author_id_1, author_id_2) < 0.3:
-            print("SIMILAIRE ",author_date_1, " ",author_date_2, " distance = ", dist_author_date(author_id_1, author_id_2))
-            print("Resultat = SIMILAIRE")
-        else:
-            print("DIFFERENT ", author_date_1, " ", author_date_2, " distance = ",dist_author_date(author_id_1, author_id_2))
-            print("Resultat = DIFFERENT")
-        #if dist_author_date(author_id_1, author_id_2) < 0.2:
-        #    print("SIMILAIRE")
-        #else:
-        #    print("Name similaire pas date")
-    #if dist_author_name(author_id_1, author_id_2) > 0.6:
-    #    print("Name different")
-    #if dist_author_date(author_id_1, author_id_2) > 0.6:
-    #    print("Date differente")
-'''
 for x in range(len(tab_id2)):
     for i in range(x + 1, len(tab_id2)):
-        # if dist_author_date(tab_id2[x],tab_id2[i]) is not None:
-        #    print(dist_author_date(tab_id2[x],tab_id2[i]))
-        #if get_author_alt_names(tab_id2[x]) is not None:
-        #    print(get_author_alt_names(tab_id2[x]))
-        # print(get_author_name(tab_id2[x]))
         if dist_author(tab_id2[x],tab_id2[i]) is not None:
             print(dist_author(tab_id2[x],tab_id2[i]))
-
-'''
-for x in range(len(tab_id2)):
-    for i in range(x + 1, len(tab_id2)):
-        #print(tab_id[x],tab_id[i])
-        #dist_author_name(tab_id[x],tab_id[i])
-        print(dist_author_date(tab_id2[x],tab_id2[i]))
-'''
-
 
 def get_author_birth_date(author_id):
     c.execute("SELECT BIRTH_DATE FROM AUTHOR WHERE ID=?", (author_id,))
@@ -280,10 +211,6 @@ def get_author_birth_date(author_id):
     return parse_db_str(d_str)
 
 
-# def get_author_death_date(author_id):
-#     ...
-
-'''
 if __name__ == '__main__':
     print(dist_year(1999, 1993))
     print(dist_year(1990, 1993))
@@ -293,89 +220,3 @@ if __name__ == '__main__':
     # print(dist_date(datetime.strptime('1990','%Y'),datetime.strptime('1990-30-04','%Y-%d-%m')))
     # print(dist_date(datetime.strptime('1990','%Y'),datetime.strptime('1991-30-04','%Y-%d-%m')))
     # print(dist_date(datetime.strptime('1990','%Y'),None))
-
-tab_alt_names = []
-def dist_name(author1,author2):
-    ed = nltk.edit_distance(author1, author2)
-    for record in csv.DictReader(open(INPUT_FILE, 'r'), fieldnames=COLUMNS, delimiter='\t'):
-        j = json.loads(record['json'])
-        #Voir comment faire avec les alternatives_names
-        #if 'alternate_names' in j:
-        #    for alt_names in j['alternate_names']:
-        #        tab_alt_names.append(nltk.edit_distance(alt_names,author1))
-        #        alt_names_min = min(tab_alt_names)
-        #        print(alt_names_min)
-
-                #print(alt_name)
-                #tab_alt_names = alt_name
-
-                #for x in range(alt_names):
-                #    for i in range(x + 1, alt_names):
-                #        ed2 = nltk.edit_distance(x, i)
-    return ed/(abs(len(author1))+abs(len(author2)))
-'''
-
-# def dist_date(author1,author2):
-#     date1 = ""
-#     date2 = ""
-#     result = ""
-#     for record in csv.DictReader(open(INPUT_FILE, 'r'), fieldnames=COLUMNS, delimiter='\t'):
-#         j = json.loads(record['json'])
-#         if 'birth_date' in j:
-#             #print(j['birth_date'], j['name'])
-#             t = (j['birth_date'],j['name'])
-#             birth_date,author = t
-#             #MEME CHOSE AVEC DEATH_DATE
-#             #On veut faire en sorte que la date corresponde à l'auteur du parametre
-#             #if l'author est egal à celui dans le parametre author1
-#             date1 = birth_date
-#             # if l'author est egal à celui dans le parametre author2
-#             date2 = birth_date
-#         if 'birth_date' in j and author1 == j['name']:
-#             date1 = j['birth_date']
-#         if 'birth_date' in j and author2 == j['name']:
-#             date2 =j['birth_date']
-#     if date1 == None:
-#         return None
-#     if date1 == None:
-#         return None
-#     #Convertir en in
-#     #new_date1 = int(date1)
-#     #new_date2 = int(date2)
-#     ed = nltk.edit_distance(date1, date2)
-#     try:
-#         result = ed/(abs(len(date1))+abs(len(date2)))
-#     except: ZeroDivisionError
-#     return result
-
-
-# valeur = []
-# valeur2 = []
-# '''
-# for x in range(len(names)):
-#     print("Looping x=%d" % x)
-#     for i in range(x + 1, len(names)):
-#         print("Looping i=%d" % i)
-#         if dist_name(names[x],names[i]) < 0.3 and dist_name(names[x],names[i])!=None:
-#             print("If condition was true")
-#             print(dist_date(names[x],names[i]))
-#         else:
-#             print("If condition was false")
-# '''
-# for x in range(len(names)):
-#     for i in range(x + 1, len(names)):
-#         if dist_name(names[x],names[i]) < 0.3 and dist_name(names[x],names[i])!=None:
-#             print(dist_date(names[x],names[i]))
-# #for z in zip(valeur,valeur2):
-
-
-# def dist_author(author_id_1,author_id_2):
-#     if dist_name(author_id_1,author_id_2) < 0.2:
-#         if dist_date(author_id_1,author_id_2) < 0.2:
-#             print("SIMILAIRE")
-#         else:
-#             print("Name similaire pas date")
-#     if dist_name(author_id_1,author_id_2) >0.6:
-#         print("Name different")
-#     if dist_date(author_id_1, author_id_2) >0.6:
-#         print("Date differente")
